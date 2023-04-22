0093fce19d2a6085d5be34b3984d3a2c0c5e8c52
viti04ek <99684929+viti04ek@users.noreply.github.com>
2023-04-23 02:17:13 +0300

    Buildings
    ADDED:
    - barack and mine models and material
    - selection indicator for buildings
    - script Building
    - script Barack
    - script Mine
    - script BuildingPlacer
    - barack and mine prefabs

---
+++ Strategy3D/Assets/Scripts/Building/Building.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : SelectableObject
+{
+    public int Price;
+    public int XSize = 3;
+    public int ZSize = 3;
+
+
+    private void OnDrawGizmos()
+    {
+        float cellSize = FindObjectOfType<BuildingPlacer>().CellSize;
+
+        for (int x = 0; x < XSize; x++)
+        {
+            for (int z = 0; z < ZSize; z++)
+            {
+                Gizmos.DrawWireCube(transform.position + new Vector3(x, 0, z) * cellSize, new Vector3(1, 0, 1) * cellSize);
+            }
+        }
+    }
+}
