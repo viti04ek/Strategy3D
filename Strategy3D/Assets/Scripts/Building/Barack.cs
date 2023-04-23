8962724bb87972d718e283b1edd641a837ce7194
viti04ek <99684929+viti04ek@users.noreply.github.com>
2023-04-24 02:03:44 +0300

    Dispelling knights on creation
    ADDED:
    - folder Unit
    
    CHANGED:
    - script Barack
    - Barack prefab

--- Strategy3D/Assets/Scripts/Building/Barack.cs
+++ Strategy3D/Assets/Scripts/Building/Barack.cs
@@ -9,6 +9,9 @@ public class Barack : Building
 
     public void CreateUnit(GameObject unitPrefab)
     {
-        Instantiate(unitPrefab, Spawn.position, Quaternion.identity);
+        GameObject newUnit = Instantiate(unitPrefab, Spawn.position, Quaternion.identity);
+
+        Vector3 position = Spawn.position + new Vector3(Random.Range(-2f, 2), 0, Random.Range(-2f, 2));
+        newUnit.GetComponent<Unit>().WhenClickOnGround(position);
     }
 }
